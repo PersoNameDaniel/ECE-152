07ef6fe01030fb1edaf5ffa2791a9697ea621c6b
PersoNameDaniel <dmolsson@outlook.com>
2022-01-12 10:21:44 -0600

    Problem 1 Finished

---
+++ ECE 152/Olsson_Lab1_p1.cpp
@@ -0,0 +1,16 @@
+/*
+Class: ECE15200
+Author: Daniel Olsson
+Assignment: Lab 1
+Date: 01-12-2022
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// printing verse
+	cout << "***** Snow flakes ***** \n \n I counted till they danced so \n Their slippers leaped the town, \n \n And then I took a pencil \n To note the rebels down." << endl;
+	return 0;
+}
---
+++ ECE 152/Olsson_Lab1_p2.cpp
@@ -0,0 +1,18 @@
+/*
+Class: ECE15200
+Author: Daniel Olsson
+Assignment: Lab 1
+Date: 01-12-2022
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	//performing computations
+	//cout << 5 + 20 - 2 << endl;
+
+
+	return 0;
+}

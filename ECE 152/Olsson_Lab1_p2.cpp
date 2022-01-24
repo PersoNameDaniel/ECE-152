2e863906da8255a874382ca9ac1e2984ca32b23a
PersoNameDaniel <dmolsson@outlook.com>
2022-01-24 13:09:31 -0600

    final

--- ECE 152/Olsson_Lab1_p2.cpp
+++ ECE 152/Olsson_Lab1_p2.cpp
@@ -10,16 +10,16 @@ Date: 01-12-2022
 using namespace std;
 
 //setting variables to computation answers
-double a = (5 + 20 - 2);
-double b = ((8 % 5) / 6);
-double c = ((8 % 5 / 6.0) + 9.5);
-double d = (6 * 3 - (3 + 6));
-double e = (3 * 8 / (4 * 3));
-double f = (3.0 * 8 / (4 * 3));
-double g = (pow(1, 3) + pow(5, 3) + pow(3, 3));
+long double a = (5 + 20 - 2);
+long double b = ((8 % 5) / 6);
+long double c = ((8 % 5 / 6.0) + 9.5);
+long double d = (6 * 3 - (3 + 6));
+long double e = (3 * 8 / (4 * 3));
+long double f = (3.0 * 8 / (4 * 3));
+long double g = (pow(1, 3) + pow(5, 3) + pow(3, 3));
 
 int main() {
-	//performing computations
+	//outputting answers
 	cout << a << endl;
 	cout << b << endl;
 	cout << c << endl;
@@ -28,7 +28,7 @@ int main() {
 	cout << f << endl;
 	cout << g << endl;
 
-
+	//cout << 9.5 + 0.3 << endl;		//test line
 
 	return 0;
 }

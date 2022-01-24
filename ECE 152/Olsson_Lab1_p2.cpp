2e83804de6b8330dd6dda9be98136ebe6e404832
PersoNameDaniel <dmolsson@outlook.com>
2022-01-24 13:05:24 -0600

    switched to assigning computations to doubles

--- ECE 152/Olsson_Lab1_p2.cpp
+++ ECE 152/Olsson_Lab1_p2.cpp
@@ -9,15 +9,24 @@ Date: 01-12-2022
 
 using namespace std;
 
+//setting variables to computation answers
+double a = (5 + 20 - 2);
+double b = ((8 % 5) / 6);
+double c = ((8 % 5 / 6.0) + 9.5);
+double d = (6 * 3 - (3 + 6));
+double e = (3 * 8 / (4 * 3));
+double f = (3.0 * 8 / (4 * 3));
+double g = (pow(1, 3) + pow(5, 3) + pow(3, 3));
+
 int main() {
 	//performing computations
-	cout << (5 + 20 - 2) << endl;
-	cout << ((8 % 5) / 6) << endl;
-	cout << ((8 % 5 / 6.0) + 9.5) << endl;
-	cout << (6 * 3 - (3 + 6)) << endl;
-	cout << (3 * 8 / (4 * 3)) << endl;
-	cout << (3.0 * 8 / (4 * 3)) << endl;
-	cout << (pow(1, 3) + pow(5, 3) + pow(3, 3)) << endl;
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+	cout << d << endl;
+	cout << e << endl;
+	cout << f << endl;
+	cout << g << endl;
 
 
 

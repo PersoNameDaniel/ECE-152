24f209a9704b76f8be2ea84db05b5538888fa41a
PersoNameDaniel <dmolsson@outlook.com>
2022-01-26 23:09:54 -0600

    Finished Lab 2 p3

--- ECE 152/Olsson_Lab2_p3.cpp
+++ ECE 152/Olsson_Lab2_p3.cpp
@@ -2,7 +2,24 @@
 
 using namespace std;
 
+//stating variables
+long double sphereRadius;			//radius of sphere that will be user input
+long double sphereSurfaceArea;		//calculated surface area value of sphere
+long double sphereVolume;			//calculated volume value of sphere
+
 int main() {
+	//user input
+	cout << "Enter the radius of the sphere: ";		//displaying question to user
+	cin >> sphereRadius;							//asking for input from user
+	cout << endl;									//skipping line for formatting
+
+	//calculation
+	sphereSurfaceArea = 4 * 3.1416 * pow(sphereRadius, 2);		//calculating surface area of sphere
+	sphereVolume = (4 / 3) * 3.1416 * pow(sphereRadius, 3);		//calculating volume of sphere
+
+	//displaying outputs
+	cout << "The surface area of a sphere with a radius of " << sphereRadius << " is: " << sphereSurfaceArea << endl;		//outputting surface area of sphere
+	cout << "The volume of a sphere with a radius of " << sphereRadius << " is: " << sphereVolume << endl;					//outputting volume of sphere
 
 	return 0;
 }

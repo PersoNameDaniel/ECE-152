88db190f2c49496a51f2f31790247571bdf1320c
PersoNameDaniel <dmolsson@outlook.com>
2022-01-27 13:15:11 -0600

    problem 2 done

--- ECE 152/Olsson_Lab3_p2.cpp
+++ ECE 152/Olsson_Lab3_p2.cpp
@@ -10,10 +10,43 @@ Date: 1-26-2022
 using namespace std;
 
 //defining variables
+long double numericalGrade;
+char letterGrade;
 
 
 int main() {
+	//getting user input
+	cout << "Please Enter numerical grade: ";		//displaying question to user
+	cin >> numericalGrade;							//receiving input
+	cout << endl;									//new line for formatting
 
+	if (numericalGrade >= 91) {										//if statement for A
+		//cout << "A" << endl;										//debug output for A
+		letterGrade = 'A';											//setting letter grade to A
+
+	} else if (numericalGrade < 91 && numericalGrade >= 81) {		//if statement for B
+		//cout << "B" << endl;										//debug output for B
+		letterGrade = 'B';											//setting letter grade to B
+
+	} else if (numericalGrade < 81 && numericalGrade >= 71) {		//if statement for C
+		//cout << "C" << endl;										//debug statement for C
+		letterGrade = 'C';											//setting letter grade to C
+
+	} else if (numericalGrade < 71 && numericalGrade >= 61) {		//if statement for D
+		//cout << "D" << endl;										//debug statement for D
+		letterGrade = 'D';											//setting letter grade to D
+
+	} else if (numericalGrade < 61) {								//if statement for F
+		//cout << "F" << endl;										//debug statement for F
+		letterGrade = 'F';											//setting letter grade for F
+
+	} else {							//else statement for error state
+		cout << "Error" << endl;		//error output
+		exit;							//stopping program
+	}
+
+	//outputting answer
+	cout << "The letter grade you got is " << letterGrade << endl;
 
 	return 0;
 }

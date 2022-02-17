18eb1c9af4bc772bfaf768112cf3cd83c67e3560
PersoNameDaniel <dmolsson@outlook.com>
2022-02-17 12:05:30 -0600

    First Test

--- ECE 152/Olsson_Lab4_p1.cpp
+++ ECE 152/Olsson_Lab4_p1.cpp
@@ -6,14 +6,33 @@ Date: 2-9-2022
 */
 
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 //defining variables
+string input;			//user input
+int inputLength;		//length of user input integer
 
 
 
 int main() {
 
+	//getting user input from user
+	cout << "Please Enter integer: ";
+	cin >> input;
+	cout << endl;
+
+	//finding length of variable, comparing first to last moving in.
+	inputLength = input.size();
+	for (int n = 0; n <= inputLength; n++) {
+		if (input.at(0 + n) != input.at(inputLength - n)) {
+			cout << "Not Palindrome" << endl;
+		exit(0);
+		}
+		else {
+			cout << "Palindrome" << endl;
+		}
+	}
 	return 0;
 }

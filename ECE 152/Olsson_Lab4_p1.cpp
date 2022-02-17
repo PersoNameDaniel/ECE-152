d39712777176708e18188017e0eb8f59145544b3
PersoNameDaniel <dmolsson@outlook.com>
2022-02-17 12:14:07 -0600

    working properly

--- ECE 152/Olsson_Lab4_p1.cpp
+++ ECE 152/Olsson_Lab4_p1.cpp
@@ -23,16 +23,21 @@ int main() {
 	cin >> input;
 	cout << endl;
 
-	//finding length of variable, comparing first to last moving in.
+	/*
+	finding length of variable, comparing first to last moving towards center
+	if test ever fails it will exit code and not display anything as asked
+	if test never fails it will exit for loop and display "*input* is a palindrome"
+	*/
+
 	inputLength = input.size();
-	for (int n = 0; n <= inputLength; n++) {
-		if (input.at(0 + n) != input.at(inputLength - n)) {
+	//cout << inputLength << endl;
+	for (int n = 0; n < (inputLength / 2); n++) {
+		if (input.at(0 + n) != input.at((inputLength - n) - 1)) {
 			cout << "Not Palindrome" << endl;
-		exit(0);
-		}
-		else {
-			cout << "Palindrome" << endl;
+			exit(0);
 		}
 	}
+	cout << input << " is a palindrome" << endl;
+	exit(0);
 	return 0;
 }

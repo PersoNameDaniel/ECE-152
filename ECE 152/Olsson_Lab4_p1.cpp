5bfef73e513aa5810af04201a70be388d8270085
PersoNameDaniel <dmolsson@outlook.com>
2022-02-17 10:51:29 -0600

    added lab 4 p1 file

---
+++ ECE 152/Olsson_Lab4_p1.cpp
@@ -0,0 +1,19 @@
+/*
+Class: ECE15200
+Author: Daniel Olsson
+Assignment: Lab 4
+Date: 2-9-2022
+*/
+
+#include <iostream>
+
+using namespace std;
+
+//defining variables
+
+
+
+int main() {
+
+	return 0;
+}

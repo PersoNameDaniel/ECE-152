999903a14bf1c7d8496d5ed9161229fb464d7914
PersoNameDaniel <dmolsson@outlook.com>
2022-02-17 12:52:33 -0600

    problem 2 first test

--- ECE 152/Olsson_Lab4_p2.cpp
+++ ECE 152/Olsson_Lab4_p2.cpp
@@ -10,9 +10,28 @@ Date: 2-9-2022
 using namespace std;
 
 //defining variables
+float startingAmount;
+float calculatedAmount;
+int years;
+float interestRate;
 
 
 int main() {
+
+	//default values
+	startingAmount = 8000;
+	years = 10;
+	interestRate = 0.025;
+
+	/*
+	
+	*/
+
+	calculatedAmount = startingAmount;
+	for (years = 0; years <= 10; years++) {
+		calculatedAmount + ((interestRate * calculatedAmount) * years);
+		cout << "After " << years << " years, there is $" << calculatedAmount << " in the account."
+	}
 
 	return 0;
 }

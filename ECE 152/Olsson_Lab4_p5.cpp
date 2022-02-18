08dd12caec16b8250830f88f23972932caa3b3a2
PersoNameDaniel <dmolsson@outlook.com>
2022-02-17 19:45:13 -0600

    I followed directions p5 done

---
+++ ECE 152/Olsson_Lab4_p5.cpp
@@ -0,0 +1,34 @@
+/*
+Class: ECE15200
+Author: Daniel Olsson
+Assignment: Lab 4
+Date: 2-9-2022
+*/
+
+#include <iostream>
+//#include <fstream>
+
+using namespace std;
+
+//defining variables
+
+
+//functions
+
+
+int main() {
+
+	//nothing to see here, I followed directions
+	cout << "1*1=1" << endl;
+	cout << "2*1=2    2*2=4" << endl;
+	cout << "3*1=3    3*2=6    3*3=9" << endl;
+	cout << "4*1=4    4*2=8    4*3=12   4*4=16" << endl;
+	cout << "5*1=5    5*2=10   5*3=15   5*4=20   5*5=25" << endl;
+	cout << "6*1=6    6*2=12   6*3=18   6*4=24   6*5=30   6*6=36" << endl;
+	cout << "7*1=7    7*2=14   7*3=21   7*4=28   7*5=35   7*6=42   7*7=49" << endl;
+	cout << "8*1=8    8*2=16   8*3=24   8*4=32   8*5=40   8*6=48   8*7=56   8*8=64" << endl;
+	cout << "9*1=9    9*2=18   9*3=27   9*4=36   9*5=45   9*6=63   9*7=63   9*8=72   9*9=81" << endl;
+
+
+	return 0;
+}

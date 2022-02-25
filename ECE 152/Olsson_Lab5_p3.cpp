c5d235e6fe949d62d358b874750fd85acc37a8b6
PersoNameDaniel <dmolsson@outlook.com>
2022-02-25 14:37:05 -0600

    p3 progress

--- ECE 152/Olsson_Lab5_p3.cpp
+++ ECE 152/Olsson_Lab5_p3.cpp
@@ -11,10 +11,36 @@ Date: 2-21-2022
 using namespace std;
 
 //defining variables
+int firstRandomNumber;
+int secondRandomNumber;
+float runningScore;
+int numberOfCorrectAnswers;
+int symbolGenerator;
 
 
 
 //functions
+
+//question generation
+int generateQuestion() {
+	firstRandomNumber = rand() % 100 + 1;
+	secondRandomNumber = rand() % 100 + 1;
+	symbolGenerator = rand() % 3 + 1;
+	switch (symbolGenerator) {
+	case 1:
+
+	break;
+
+	case 2:
+
+	break;
+
+	case 3:
+
+	break;
+
+	}
+}
 
 
 int main() {

ff850201843752564ec48f3215b657cb4695b07b
PersoNameDaniel <dmolsson@outlook.com>
2022-03-02 14:33:24 -0600

    p6 done pre-purge

--- ECE 152/Olsson_Lab5_p6.cpp
+++ ECE 152/Olsson_Lab5_p6.cpp
@@ -11,11 +11,47 @@ Date: 2-21-2022
 using namespace std;
 
 //defining variables
+int input;
 
 //functions
+int sumFibonacci(int n) {
+	//int newNum = 0;
+	//int oldNum = 1;
+	if (n == 0) {
+		return 0;
+	}
+	if (n == 1 or n == 2) {
+		return 1;
+	}
+
+	int output;
+	int numbers[999];
+	for (int i = 2; i <= n; i++) {
+		//newNum++;
+		//oldNum = newNum + oldNum;
+		
+		numbers[0] = 0;
+		numbers[1] = 1;
+		numbers[2] = 1;
+
+		numbers[i] = (numbers[i - 2] + numbers[i - 1]);
+
+		output = numbers[i];
+
+	}
+	return output;
+
+}
 
 //main function
 int main() {
+	cout << "Enter positive integer to calculate fibonacci sequence to." << endl;
+	cin >> input;
+	cout << endl;
+
+	int n = input;
+
+	cout << sumFibonacci(n) << endl;
 
 	return 0;
 }

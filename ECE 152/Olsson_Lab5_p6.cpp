f9d44c79067a0d435071ea645bf4bfa1f1a367bb
PersoNameDaniel <dmolsson@outlook.com>
2022-03-02 13:53:32 -0600

    p6 templated

---
+++ ECE 152/Olsson_Lab5_p6.cpp
@@ -0,0 +1,21 @@
+/*
+Class: ECE15200
+Author: Daniel Olsson
+Assignment: Lab 5
+Date: 2-21-2022
+*/
+
+#include <iostream>
+//#include <fstream>
+
+using namespace std;
+
+//defining variables
+
+//functions
+
+//main function
+int main() {
+
+	return 0;
+}

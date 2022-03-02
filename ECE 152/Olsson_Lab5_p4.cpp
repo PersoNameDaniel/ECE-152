13377fe759acd0577fb2ae6b9154cdd565f3e942
PersoNameDaniel <dmolsson@outlook.com>
2022-03-02 12:14:30 -0600

    P4 done P5 template

--- ECE 152/Olsson_Lab5_p4.cpp
+++ ECE 152/Olsson_Lab5_p4.cpp
@@ -11,13 +11,34 @@ Date: 2-21-2022
 using namespace std;
 
 //defining variables
-
+int randomNumber;
+bool guessed;
+int guess;
 
 
 //functions
 
 
 int main() {
+	randomNumber = rand() % 99 + 1;
+	guessed = false;
+	cout << "I have a number between 1 and 100." << endl << "Can you guess my number?" << endl << "Please type your first guess: ";
+	while (guessed == false) {
+		cin >> guess;
+		cout << endl;
+
+		if (guess == randomNumber) {
+			guessed = true;
+			cout << "Excellent! You guessed the number!";
+		}
+		else if (guess <= randomNumber) {
+			cout << "Too low. Try again." << endl;
+		}
+		else if (guess >= randomNumber) {
+			cout << "Too high. Try again." << endl;
+		}
+	}
+	
 
 
 	return 0;

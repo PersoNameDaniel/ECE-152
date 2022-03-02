32c818f7652d1fb871a4468b7d36f66ba9c6074e
PersoNameDaniel <dmolsson@outlook.com>
2022-03-02 13:30:38 -0600

    i don tknow whats going on

--- ECE 152/Olsson_Lab5_p5.cpp
+++ ECE 152/Olsson_Lab5_p5.cpp
@@ -6,17 +6,58 @@ Date: 2-21-2022
 */
 
 #include <iostream>
+//#include <tuple>
 //#include <fstream>
 
 using namespace std;
 
 //defining variables
+int input1;
+int input2;
+int input3;
 
 //functions
+int* minMax(int number1, int number2, int number3) {
 
+	int numbers[3] = { number1, number2, number3 };
 
+	int max = numbers[0];
+	for (int i = 0; i < 3; i++) {
+
+		if (max < numbers[i]) {
+			max = numbers[i];
+		}
+	}
+
+	int min = numbers[0];
+	for (int i = 0; i < 3; i++) {
+
+		if (min > numbers[i]) {
+			min = numbers[i];
+		}
+	}
+
+	int arrayOutput[2] = {min, max};
+
+	//tuple <int, int> tupleOutput;
+	//tupleOutput = make_tuple(min, max);
+
+	return arrayOutput;
+}
+
+//main function
 int main() {
+	cout << "Enter integer 1: ";
+	cin >> input1;
+	cout << endl << "Enter integer 2: ";
+	cin >> input2;
+	cout << endl << "Enter integer 3: ";
+	cin >> input3;
+	cout << endl;
 
+	int* ptr = minMax(input1, input2, input3);
+
+	cout << "Minimum number was" << *ptr[0] << "Maximum number was" << *ptr[1] << endl;
 
 	return 0;
 }

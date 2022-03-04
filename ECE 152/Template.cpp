a07bba1967e475a8e4e9d1d16531ae9f3e5c5744
PersoNameDaniel <dmolsson@outlook.com>
2022-03-04 12:33:39 -0600

    Lab 6 Template

--- ECE 152/Template.cpp
+++ ECE 152/Template.cpp
@@ -1,18 +1,19 @@
 /*
 Class: ECE15200
 Author: Daniel Olsson
-Assignment: Lab 2
-Date: 1-26-2022
+Assignment: Lab 6
+Date: 2-28-2022
 */
 
 #include <iostream>
 
 using namespace std;
 
-//stating variables
+//defining variables
+
+//functions
 
 int main() {
-
 
 	return 0;
 }

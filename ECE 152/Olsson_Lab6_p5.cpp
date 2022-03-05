6b89f1325f55e90dbd0764617a38b4d0dac20483
PersoNameDaniel <dmolsson@outlook.com>
2022-03-04 22:06:44 -0600

    p5

---
+++ ECE 152/Olsson_Lab6_p5.cpp
@@ -0,0 +1,19 @@
+/*
+Class: ECE15200
+Author: Daniel Olsson
+Assignment: Lab 6
+Date: 2-28-2022
+*/
+
+#include <iostream>
+
+using namespace std;
+
+//defining variables
+int scores[4][3] = {{96, 80, 89}, {76, 93, 85}, {66, 70, 82}, {90, 79, 87}};		//John = 0, Julie = 1, Kevin = 2, David = 3
+//functions
+
+int main() {
+
+	return 0;
+}

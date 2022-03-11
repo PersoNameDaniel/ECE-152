6342089e1126929037af7564159b46a78858c1ac
PersoNameDaniel <dmolsson@outlook.com>
2022-03-10 21:57:42 -0600

    L7P3 array building done

--- ECE 152/Olsson_Lab7_p3.cpp
+++ ECE 152/Olsson_Lab7_p3.cpp
@@ -10,10 +10,32 @@ Date: 3-07-2022
 using namespace std;
 
 //defining variables
+float studentGradeArray[3][4];
 
 //functions
 
 int main() {
+
+	//seeding rand
+	srand(time(0));
+
+	//populating slightly less massive array
+	for (int i = 0; i <= 2; i++) {
+		for (int j = 0; j <= 3; j++) {
+			studentGradeArray[i][j] = rand() % 61 + 40;
+		}
+	}
+
+	//uncomment below to display entire array
+
+	
+	for (int i = 0; i <= 2; i++) {
+		cout << endl;
+		for (int j = 0; j <= 3; j++) {
+			cout << studentGradeArray[i][j] << ", ";
+		}
+	}
+	
 
 	return 0;
 }

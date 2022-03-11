707dd0c39b58e67058d965f8375546023331bbb5
PersoNameDaniel <dmolsson@outlook.com>
2022-03-10 20:56:43 -0600

    L7P1 done

--- ECE 152/Olsson_Lab7_p1.cpp
+++ ECE 152/Olsson_Lab7_p1.cpp
@@ -12,9 +12,8 @@ using namespace std;
 //defining variables
 int userIntArray[2][4];
 int arrayRow1Total = 0;
-int arrayRow1Average = 0;
+int arrayRowAverage[2] = { 0, 0 };
 int arrayRow2Total = 0;
-int arrayRow2Average = 0;
 
 //functions
 
@@ -33,12 +32,23 @@ int main() {
 	for (int i = 0; i <= 3; i++) {
 		arrayRow1Total = userIntArray[0][i] + arrayRow1Total;
 	}
-	arrayRow1Average = (arrayRow1Total / 4);
+	arrayRowAverage[0] = (arrayRow1Total / 4);
 
 	for (int i = 0; i <= 3; i++) {
 		arrayRow2Total = userIntArray[1][i] + arrayRow2Total;
 	}
-	arrayRow2Average = (arrayRow2Total / 4);
+	arrayRowAverage[1] = (arrayRow2Total / 4);
+
+	//displaying resopnses
+	for (int i = 0; i <= 1; i++) {
+		for (int j = 0; j <= 3; j++) {
+			cout << "Row " << (i + 1) << " position " << (j + 1) << " is: " << userIntArray[i][j] << endl;
+		}
+	}
+
+	for (int i=0;i<=1;i++) {
+		cout << "Row " << (i + 1) << " average is " << arrayRowAverage[i] << endl;
+	}
 
 	return 0;
 }

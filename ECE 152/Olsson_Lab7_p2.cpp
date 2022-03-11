25d63255ed330256bec332b6bceac4537a84b6d2
PersoNameDaniel <dmolsson@outlook.com>
2022-03-10 21:09:50 -0600

    L7P2 function findmax done

--- ECE 152/Olsson_Lab7_p2.cpp
+++ ECE 152/Olsson_Lab7_p2.cpp
@@ -10,8 +10,30 @@ Date: 3-07-2022
 using namespace std;
 
 //defining variables
+int userArray[10][15];
 
 //functions
+void findmax() {
+
+	//defining function variables
+	int max = 0;
+	int maxRow;
+	int maxColumn;
+
+	//finding max integer
+	for (int i = 0; i <= 9; i++) {
+		for (int j = 0; j <= 14; j++) {
+			if (max < userArray[i][j]) {
+				max = userArray[i][j];
+				maxRow = i;
+				maxColumn = j;
+			}
+		}
+	}
+	//displaying result
+	cout << "Max integer in array is: " << max << endl;
+	cout << max << " is located at " << maxRow << ", " << maxColumn << endl;
+}
 
 int main() {
 

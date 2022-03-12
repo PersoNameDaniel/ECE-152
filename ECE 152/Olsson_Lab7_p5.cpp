966429df81f837cf056178ba0a25d070c62de5ab
PersoNameDaniel <dmolsson@outlook.com>
2022-03-11 22:20:32 -0600

    L7P5 stuck

--- ECE 152/Olsson_Lab7_p5.cpp
+++ ECE 152/Olsson_Lab7_p5.cpp
@@ -12,8 +12,16 @@ using namespace std;
 //defining variables
 
 //functions
+void show(float (*rates)[9]) {
+	for (int i = 0; i <= 8; i++) {
+		cout << "value number " << (i + 1) << " is: " << *(rates + i) << endl;
+	}
+}
 
 int main() {
+	float rates[9] = {6.5, 7.2, 7.5, 8.7, 8.6, 9.4, 9.6, 9.8, 10.6};
+
+	show(&rates);
 
 	return 0;
 }

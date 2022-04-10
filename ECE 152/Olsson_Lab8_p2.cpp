b53876070064eb4c687a6d6212f80a994c22677c
PersoNameDaniel <dmolsson@outlook.com>
2022-04-10 14:27:54 -0500

    L8P2 finish problem 2

--- ECE 152/Olsson_Lab8_p2.cpp
+++ ECE 152/Olsson_Lab8_p2.cpp
@@ -10,10 +10,18 @@ Date: 04-05-2022
 using namespace std;
 
 //defining variables
-
+int miles[7] = {15, 22, 16, 18, 27, 23, 20};
+int dist[7];
 //functions
 
 int main() {
+	for (int i = 0; i <= 6; i++) {
+		dist[i] = *(miles + i);
+	}
+
+	for (int i = 0; i <= 6; i++) {
+		cout << "array 'dist' position " << i << " is: " << dist[i] << endl;
+	}
 
 	return 0;
 }

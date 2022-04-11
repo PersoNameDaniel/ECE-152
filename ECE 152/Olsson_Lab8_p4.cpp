9a70249daa466d37b28b21dd5e2395f3f87629a6
PersoNameDaniel <dmolsson@outlook.com>
2022-04-10 20:40:40 -0500

    L8P4 Finish

--- ECE 152/Olsson_Lab8_p4.cpp
+++ ECE 152/Olsson_Lab8_p4.cpp
@@ -10,10 +10,23 @@ Date: 04-05-2022
 using namespace std;
 
 //defining variables
+char str[37] = {'C', 'p', 'l', 'u', 's', 'p', 'l', 'u', 's', 'i', 's', 'a', 's', 'i', 'm', 'p', 'l', 'e',
+	'p', 'r', 'o', 'g', 'r', 'a', 'm', 'm', 'i', 'n', 'g', 'l', 'a', 'n', 'g', 'u', 'a', 'g', 'e'};
+char Pt[37];
+int numberOfp = 0;
 
 //functions
 
 int main() {
+	for (int i = 0; i <= 36; i++) {
+		Pt[i] = *(str + i);
+	}
 
+	for (int i = 0; i <= 36; i++) {
+		if (Pt[i] == 'p') {
+			numberOfp++;
+		}
+	}
+	cout << "There are " << numberOfp << " 'p's in the string." << endl;
 	return 0;
 }

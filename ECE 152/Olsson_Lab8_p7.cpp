f64571c7d6656f6ddb7a9feb4d864d48e59d6d72
PersoNameDaniel <dmolsson@outlook.com>
2022-04-10 21:03:39 -0500

    L8P7 Created

---
+++ ECE 152/Olsson_Lab8_p7.cpp
@@ -0,0 +1,19 @@
+/*
+Class: ECE15200
+Author: Daniel Olsson
+Assignment: Lab 8
+Date: 04-05-2022
+*/
+
+#include <iostream>
+
+using namespace std;
+
+//defining variables
+
+//functions
+
+int main() {
+
+	return 0;
+}

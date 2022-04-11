247347aef2af77424c7a5ea4b554e73e6260d269
PersoNameDaniel <dmolsson@outlook.com>
2022-04-10 22:51:05 -0500

    L8P3 working, but messy

--- ECE 152/Olsson_Lab8_p3.cpp
+++ ECE 152/Olsson_Lab8_p3.cpp
@@ -13,30 +13,40 @@ using namespace std;
 string userInput;
 char userArray[5];
 
+void reverse(char s[]);
 
-//functions
-void reverse(char s[]) {
-	char reverseString[5];
-	for (int i = 0; i < size(userInput); i++) {
-		reverseString[(size(userInput) - i)] = s[i];
-		cout << "f1 " << i << endl;
-	}
-
-	cout << "string reversed: " << endl;
-	for (int i = 0; i < size(userInput); i++) {
-		cout << reverseString[i];
-	}
-}
 
 int main() {
 	cout << "Please enter word or phrase to reverse" << endl;
 	cin >> userInput;
 
 	for (int i = 0; i < size(userInput); i++) {
-		userArray[i];
+		userArray[i] = userInput.at(i);
 	}
+
+	/*char inputAsArray[userInput.size() + 1];
+	strncpy(inputAsArray, userInput.c_str(), userInput.size() + 1);
+
+	reverse(userInput.c_str());*/
 
 	reverse(userArray);
 
 	return 0;
+}
+
+//functions
+void reverse(char s[]) {
+	int arraySize = (sizeof(s) / sizeof(s[0]));
+	//int arraySize = 5;
+	cout << "arraySize = " << arraySize << endl;
+	char reverseString[5] = {};
+	for (int i = 0; i < arraySize; i++) {
+		reverseString[arraySize - i] = s[i];
+		cout << "s[" << i << "] = " << s[i] << endl;
+	}
+
+	cout << "string reversed: " << endl;
+	for (int i = 0; i < arraySize + 1; i++) {
+		cout << reverseString[i];
+	}
 }

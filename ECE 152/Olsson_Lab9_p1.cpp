3f53573785fe12d911d46695667d88e411cf0d22
PersoNameDaniel <dmolsson@outlook.com>
2022-04-12 15:39:15 -0500

    L9P1 Finish p1

--- ECE 152/Olsson_Lab9_p1.cpp
+++ ECE 152/Olsson_Lab9_p1.cpp
@@ -10,10 +10,29 @@ Date: 04-12-2022
 using namespace std;
 
 //defining variables
+struct {
+	string month;
+	string day;
+	int year;
+} user;
 
 //functions
 
 int main() {
+
+	// asking for user input
+	cout << "What is the current month? ";
+	cin >> user.month;
+
+	cout << "What is the current day? ";
+	cin >> user.day;
+
+	cout << "What is the current year? ";
+	cin >> user.year;
+
+	// outputting date
+	cout << "The date entered by user is: " << endl;
+	cout << user.month << " " << user.day << ", " << user.year << endl;
 
 	return 0;
 }

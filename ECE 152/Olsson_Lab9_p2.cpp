6510468354cbd7ed504b3096f046a42343a80242
PersoNameDaniel <dmolsson@outlook.com>
2022-04-12 15:40:01 -0500

    L9P2 create and format problem 2

---
+++ ECE 152/Olsson_Lab9_p2.cpp
@@ -0,0 +1,19 @@
+/*
+Class: ECE15200
+Author: Daniel Olsson
+Assignment: Lab 9
+Date: 04-12-2022
+*/
+
+#include <iostream>
+
+using namespace std;
+
+//defining variables
+
+//functions
+
+int main() {
+
+	return 0;
+}

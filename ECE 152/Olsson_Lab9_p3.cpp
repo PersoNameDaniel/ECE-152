02184d6e290a96546401ea3488de81ca59ae5b2c
PersoNameDaniel <dmolsson@outlook.com>
2022-04-15 22:35:40 -0500

    L9P3 Finish

--- ECE 152/Olsson_Lab9_p3.cpp
+++ ECE 152/Olsson_Lab9_p3.cpp
@@ -10,10 +10,45 @@ Date: 04-12-2022
 using namespace std;
 
 // defining variables
+struct Date {
+	int month;
+	int day;
+	int year;
+};
+
+Date current;
+int month;
+int day;
+int year;
 
 // functions
+int days(int userMonth, int userDay, int userYear) {
+	current.month= userMonth;
+	current.day = userDay;
+	current.year = userYear;
+
+	current.year = (current.year - 1900);
+
+	current.month = (current.month + (current.year * 12));
+	current.day = (current.day + (current.month * 30));
+
+	return current.day;
+}
 
 int main() {
+	Date userInput;
+
+	cout << "Please enter numerical month to calculate" << endl;
+	cin >> userInput.month;
+	
+	cout << "Please enter numerical day to calculate" << endl;
+	cin >> userInput.day;
+
+	cout << "Please enter numerical year to calculate" << endl;
+	cin >> userInput.year;
+
+	cout << "There have been " << days(userInput.month, userInput.day, userInput.year) <<
+		" days since January 1, 1900." << endl;
 
 	return 0;
 }

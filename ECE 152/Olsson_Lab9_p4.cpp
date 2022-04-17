28324357724313bc6013e66246eeddcd198e6933
PersoNameDaniel <dmolsson@outlook.com>
2022-04-17 14:28:46 -0500

    L9P4 continued

--- ECE 152/Olsson_Lab9_p4.cpp
+++ ECE 152/Olsson_Lab9_p4.cpp
@@ -7,6 +7,8 @@ Date: 04-12-2022
 
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
+#include <iomanip>
 
 using namespace std;
 
@@ -17,9 +19,19 @@ using namespace std;
 int main() {
 	ifstream inFile;
 	inFile.open("employees.dat");
+	if (inFile.fail()) {
+		cout << "File failed to open" << endl;
+		exit(1);
+	}
 
 	ofstream outFile;
 	outFile.open("employee.bak");
+	if (outFile.fail()) {
+		cout << "File failed to open" << endl;
+		exit(1);
+	}
+
+	outFile << setiosflags(ios::fixed) << setiosflags(ios::showpoint) << setprecision(2);
 
 	return 0;
 }

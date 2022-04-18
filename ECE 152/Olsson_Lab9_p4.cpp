57ef23483f180fa7aed52f7ca92e273ce039bfea
PersoNameDaniel <dmolsson@outlook.com>
2022-04-18 12:07:41 -0500

    L9P4 pausing for now, running example file

--- ECE 152/Olsson_Lab9_p4.cpp
+++ ECE 152/Olsson_Lab9_p4.cpp
@@ -5,9 +5,11 @@ Assignment: Lab 9
 Date: 04-12-2022
 */
 
+// #includes
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <string>
 #include <iomanip>
 
 using namespace std;
@@ -18,7 +20,7 @@ using namespace std;
 
 int main() {
 	ifstream inFile;
-	inFile.open("employees.dat");
+	inFile.open("employee.dat");
 	if (inFile.fail()) {
 		cout << "File failed to open" << endl;
 		exit(1);
@@ -30,6 +32,14 @@ int main() {
 		cout << "File failed to open" << endl;
 		exit(1);
 	}
+
+	// send data to the file
+	outFile << "Mats " << 39.95 << endl
+		<< "Bulbs " << 3.022 << endl
+		<< "Fuses " << 1.08 << endl;
+
+	outFile.close();
+	cout << "The file " << "employee.bak" << " has been successfully written." << endl;
 
 	outFile << setiosflags(ios::fixed) << setiosflags(ios::showpoint) << setprecision(2);
 

e782bae9ca4bf3481795c1e6e34e6605a7aa76a0
PersoNameDaniel <dmolsson@outlook.com>
2022-04-23 23:09:02 -0500

    L9P5 Finish

--- ECE 152/Olsson_Lab9_p5.cpp
+++ ECE 152/Olsson_Lab9_p5.cpp
@@ -16,10 +16,53 @@ Date: 04-12-2022
 using namespace std;
 
 // defining variables
+int totalMiles;
+int totalGallons;
+double totalMPG;
 
 // functions
+int fileInput[5][4];
 
 int main() {
+	ifstream inFile;
+	inFile.open("carSpecifications.dat");
+	if (inFile.fail()) {
+		cout << "File failed to open" << endl;
+		exit(1);
+	}
+
+	for (int i = 0; i <= 4; i++) {
+		for (int j = 0; j <= 2; j++) {
+			inFile >> fileInput[i][j];
+		}
+	}
+
+	for (int i = 0; i <= 4; i++) {
+		fileInput[i][3] = (fileInput[i][1] / fileInput[i][2]);
+	}
+
+	totalMiles = 0;
+	for (int i = 0; i <= 4; i++) {
+		totalMiles = (fileInput[i][1] + totalMiles);
+	}
+
+	totalGallons = 0;
+	for (int i = 0; i <= 4; i++) {
+		totalGallons = (fileInput[i][2] + totalGallons);
+	}
+
+	cout << "ID   Miles  Gallons  MPG" << endl;
+	for (int i = 0; i <= 4; i++) {
+		cout << endl;
+		for (int j = 0; j <= 3; j++) {
+			cout << fileInput[i][j] << "    ";
+		}
+	}
+
+	cout << endl;
+	cout << "Total Miles Driven: " << totalMiles << endl;
+	cout << "Total Gallons Used: " << totalGallons << endl;
+	cout << "Total MPG: " << (totalMiles / totalGallons) << endl;
 
 	return 0;
 }
